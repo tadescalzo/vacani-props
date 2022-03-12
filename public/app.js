cf89d6fd4e35da906286884348bc594f623ddf29
Nacho <ta.descalzo@gmail.com>
2022-03-12 00:10:55 -0300

    first commit

---
+++ public/app.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBh9sET5AqFOtq-pVbD3zih_M3-88IZenc",
+  authDomain: "vacani-props.firebaseapp.com",
+  projectId: "vacani-props",
+  storageBucket: "vacani-props.appspot.com",
+  messagingSenderId: "280501608482",
+  appId: "1:280501608482:web:805b732642b7b0656f2a7b",
+  measurementId: "G-44KFFXT7VP",
+};
+
+firebase.initializeApp(firebaseConfig);
